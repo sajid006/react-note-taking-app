6036ba1eda7b5b5e1a67b2da01b4a34719ebbc28
Sajid Hasan <sajid.hasan@cefalo.com>
2022-12-27 17:24:19 +0600

    First commit

---
+++ src/NoteLayout.tsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet, useOutletContext, useParams } from "react-router";
+import { Note } from "./App";
+
+type NoteLayoutProps = {
+  notes: Note[];
+};
+export function NoteLayout({ notes }: NoteLayoutProps) {
+  const { id } = useParams();
+  const note = notes.find((n) => n.id === id);
+
+  if (note == null) return <Navigate to="/" replace />;
+  return <Outlet context={note} />;
+}
+export function useNote() {
+  return useOutletContext<Note>();
+}
